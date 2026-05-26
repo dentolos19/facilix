93e4795c8938b9d1bea6cc0daf9ce1e971d0d588
Dennise Catolos <dentolos19@gmail.com>
2026-05-26 21:41:29 +0800

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+const config = defineConfig({
+  server: {
+    port: 3000,
+    allowedHosts: [".dennise.me"],
+  },
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [cloudflare({ viteEnvironment: { name: "ssr" } }), tanstackStart(), viteReact(), tailwindcss(), devtools()],
+});
+
+export default config;
